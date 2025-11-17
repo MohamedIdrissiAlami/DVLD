4f2a27834d1b45d6e20a97c556424c815d3d97b5
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-11-17 23:18:40 +0100

    DVLD-1 Creating the project structure with its three Layers

---
+++ DVLD_Business_Layer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLD_Data_Access_Layer;
+namespace DVLD_Business_Layer
+{
+    public class Class1
+    {
+    }
+}
